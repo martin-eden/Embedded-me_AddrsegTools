9a732b05db3c3fda143a6894ec1f35b3b7fbea01
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2025-08-22 20:09:50 +0200

    [.~] [Example] Sync with current codebase.

--- examples/me_MemorySegment/me_MemorySegment.ino
+++ examples/me_MemorySegment/me_MemorySegment.ino
@@ -2,7 +2,7 @@
 
 /*
   Author: Martin Eden
-  Last mod.: 2024-12-20
+  Last mod.: 2025-08-22
 */
 
 /*
@@ -19,12 +19,11 @@
 #include <me_MemorySegment.h>
 
 #include <me_BaseTypes.h>
-#include <me_Uart.h>
 #include <me_Console.h>
 
 void setup()
 {
-  me_Uart::Init(me_Uart::Speed_115k_Bps);
+  Console.Init();
 
   Console.Print("[me_MemorySegment] Okay, we are here.");
   Console.Indent();
